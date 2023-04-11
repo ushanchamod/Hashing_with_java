20f5f3f5d22e2c57880c5707896849e0bdd08f49
ushanchamod <ushanchamodbandara@gmail.com>
2023-04-11 21:28:24 +0530

    Untracked files: 	DATA_STRACTURES/HashTable/ 	EEX4465-2022-23_MP.pdf 	HT/ 	outputs/wordsHK3.txt 	outputs/wordsQHK3.txt 	source/file1.txt 	source/file2.txt 	source/file4.txt 	source/file5.txt 	source/file6.txt 	source/file7.txt

--- Main.java
+++ Main.java
@@ -1,20 +1,37 @@
-import java.util.Arrays;
-
 import HK.HashKey;
+import HT.Hash_table;
 import LinkedList.LinkedList;
 import text_file_control.Txt_read;
-import text_file_control.Txt_write;
 
 public class Main {
     public static void main(String[] args) {
-        Txt_read tr = new Txt_read();
-        Txt_write tw = new Txt_write();
 
-        LinkedList<String> word_list = tr.getWordList("source/file3.txt");
+        ///////////////////////////// task 1 ////////////////////////////////////////
 
-        // Object[] words = word_list.toArray();
-        HashKey hk = new HashKey(word_list.toArray());
-        hk.generate_wordHK_txt();
+        String file_name_for_generate_hk = "file3.txt"; // for first input
+        String hash_key_table_output_fileName = "wordsHK3.txt"; // for first output
+
+        Txt_read tr = new Txt_read(); // read files object
+
+        // read file and get word list & generate hash key
+        HashKey hashKeyObj = new HashKey(tr.getWordList("source/" + file_name_for_generate_hk).toArray(), hash_key_table_output_fileName);
+        hashKeyObj.generate_wordHK_txt(); // generate word hash key txt file
+
+
+        ///////////////////////////// task 2 ////////////////////////////////////////
+
+        Txt_read tr2 = new Txt_read();
+        
+        String file_name_two = "wordsHK3.txt"; // for second input
+        String output = "wordsQHKn.txt"; // for second output
+
+        
+        Hash_table hashTable = new Hash_table(tr2.getWordList("outputs/" + file_name_two));
+        hashTable.createHashTable(output);
+
+
+        ///////////////////////////// task 3 ////////////////////////////////////////
+        
 
     }
 }
